64e4159600eeefa716f4674dbaafeb76f0fb0627
Elonpop-make <82283481+Elonpop-make@users.noreply.github.com>
2023-04-30 21:37:44 -0400

    Create test.c

---
+++ test.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <termios.h>
+#include <flipper/flipper.h>
+
+#define DOT_DURATION_MS 200 // Duration of a dot in milliseconds
+#define DASH_DURATION_MS 3 * DOT_DURATION_MS // Duration of a dash in milliseconds
+#define PAUSE_DURATION_MS DOT_DURATION_MS // Duration of a pause between dots and dashes in milliseconds
+#define LETTER_PAUSE_DURATION_MS 3 * DOT_DURATION_MS // Duration of a pause between letters in milliseconds
+
+void morse_code(char c) {
+    switch (c) {
+        case 'a':
+            lf_tone_play(440, DOT_DURATION_MS); // Play A as a dot
+            lf_delay(PAUSE_DURATION_MS);
+            lf_tone_stop();
+            break;
+        case 'b':
+            lf_tone_play(440, DASH_DURATION_MS); // Play B as a dash
+            lf_delay(PAUSE_DURATION_MS);
+            lf_tone_stop();
+            break;
+        // Other Morse code mappings for letters here...
+        default:
+            // No Morse code mapping for this character
+            break;
+    }
+}
+
+int main(void) {
+    lf_return_t ret;
+
+    // Initialize Flipper hardware
+    lf_initialize();
+
+    // Configure tone output pin
+    lf_tone_configure();
+
+    // Configure standard input
+    struct termios tio;
+    tcgetattr(STDIN_FILENO, &tio);
+    tio.c_lflag &= ~ICANON;
+    tio.c_cc[VMIN] = 1;
+    tcsetattr(STDIN_FILENO, TCSANOW, &tio);
+
+    // Read input from standard input and generate Morse code sounds
+    while (1) {
+        char c = getchar();
+        if (c == EOF) {
+            break;
+        }
+        morse_code(c);
+        lf_delay(LETTER_PAUSE_DURATION_MS);
+    }
+
+    // Restore standard input configuration
+    tcgetattr(STDIN_FILENO, &tio);
+    tio.c_lflag |= ICANON;
+    tcsetattr(STDIN_FILENO, TCSANOW, &tio);
+
+    // Deinitialize Flipper hardware
+    lf_deinitialize();
+
+    return 0;
+}
